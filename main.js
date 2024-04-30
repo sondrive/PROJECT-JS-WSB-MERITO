c260131a16643e321ec875f7242d169c8efa81ba
sondrive <synowieczki@gmail.com>
2024-04-30 19:44:42 +0200

    ADD PROJECT JS

--- main.js
+++ main.js
@@ -1,34 +1,58 @@
-// Pobieramy wszystkie przyciski "Kup teraz"
+// Pobieramy wszystkie przyciski potrzebne w projekcie
 const buyButtons = document.querySelectorAll(".buyButton");
+const backBtn = document.querySelector(".backBtn");
+const finishBuy = document.querySelector(".finishBuy");
+const backBtnFinal = document.querySelector(".backBtnFinal");
 
 // Pobieramy całą sekcję produktów oraz formularz
 const productSection = document.querySelector(".product_section");
 const formSection = document.querySelector(".form");
 const finalSection = document.querySelector(".finally_section");
 
-//Pobieramy spany w których ma się znajdować wartość z klikniętego kafla produktu
+// Pobieramy spany w których ma się znajdować wartość z klikniętego kafla produktu
 const selectedMachine = document.querySelector(".selected_machine");
 const basicPrice = document.querySelector(".final_price");
 const yourChoice = document.querySelector(".choice");
 const addItems = document.getElementById("addGPS");
 const addItems2 = document.getElementById("addCamera");
 
-// Iterujemy przez każdy przycisk i dodajemy nasłuchiwanie zdarzenia kliknięcia
+// Pobieramy spany z błędami w formularzu
+
+const name = document.getElementById("fname");
+const errorName = document.querySelector(".nameError");
+const dataName = document.querySelector(".dataError");
+const paymentError = document.querySelector(".paymentError");
+
+// Pobieramy przyciski dodawania akcesorii
+
+const addGps = document.getElementById("addGps");
+const subGps = document.getElementById("subGps");
+const addCamera = document.getElementById("addCamera");
+const subCamera = document.getElementById("subCamera");
+
+// Pobieramy div ze zdjeciem finalnym
+const machineImg = document.querySelector(".our_machine_photo");
+
+// Iterujemy przez każdy przycisk i dodajemy nasłuchiwanie zdarzenia kliknięcia za pomocą forEach
 buyButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    // Pobieramy informacje o maszynie na podstawie przycisku "Kup teraz"
-    const productDescription = button.parentElement;
+    // Pobieramy informacje o maszynie na podstawie przycisku "Kup teraz" z div product_cart
+    const productCart = button.closest(".product_cart");
 
-    const brand = productDescription.querySelector(".brand").textContent;
-    const model = productDescription.querySelector(".model").textContent;
-    const price = productDescription.querySelector(".price").textContent;
+    const brand = productCart.querySelector(".brand").textContent;
+    const model = productCart.querySelector(".model").textContent;
+    const price = productCart.querySelector(".price").textContent;
+    const image = productCart.querySelector("img").src;
 
-    //implementujemy w spany wartości z obiektu
+    // Tu wyrzucam zmienna do global
+    window.image = image;
+
+    // Implementujemy w spany wartości z obiektu
     selectedMachine.innerHTML = `${brand} ${model}`;
     yourChoice.innerHTML = `${brand} ${model}`;
     basicPrice.innerHTML = `${Number(price)}`;
 
-    // ukrywamy sekcję product_section
+    // Ukrywamy sekcję product_section
     productSection.classList.add("hidden");
     productSection.classList.remove("visible");
     formSection.classList.add("visible");
@@ -38,12 +62,10 @@ buyButtons.forEach((button) => {
 
 // ============================================== FORMS
 
-// Add Accessories
+const radioButton1 = document.getElementById("cash");
+const radioButton2 = document.getElementById("leasing");
 
-const addGps = document.getElementById("addGps");
-const subGps = document.getElementById("subGps");
-const addCamera = document.getElementById("addCamera");
-const subCamera = document.getElementById("subCamera");
+// Dodajemy kwote w zależności od klikniętego + lub - . Ustawiamy flage
 
 let gpsIsAdded = true;
 let cameraIsAdded = true;
@@ -55,7 +77,6 @@ addGps.addEventListener("click", () => {
     addGps.classList.add("green");
     gpsIsAdded = false;
     finalPrice = basicPrice;
-    console.log("dupa");
   }
 });
 
@@ -84,9 +105,9 @@ subCamera.addEventListener("click", () => {
   }
 });
 
-// =============================================== Przemieszczanie sie po stronie
-// Przycisk wróć do produktów
-const backBtn = document.querySelector(".backBtn");
+// =============================================== Przemieszczanie sie po stronie oraz wykonywanie podstawowych funkcji strony
+
+// Przycisk wróć do produktów i reset
 
 backBtn.addEventListener("click", () => {
   productSection.classList.add("visible");
@@ -98,28 +119,58 @@ backBtn.addEventListener("click", () => {
   cameraIsAdded = true;
 });
 
-// Pobieranie przycisku "Kup"
-const finishBuy = document.querySelector(".finishBuy");
-const name = document.getElementById("fname");
-const errorName = document.querySelector(".nameError");
+// Funkcja przycisku "Kup"
 
 finishBuy.addEventListener("click", () => {
-  // Check Name and Surname
+  // Sprawdzanie imienia i nazwiska
   const value = name.value;
+
+  // Nie ukrywam, że to znalazłem w internecie :))
   const regex = /^[a-zA-Z]+\s[a-zA-Z]+$/;
 
-  if (value.includes(" ") && regex.test(value)) {
+  // Pobieranie aktualnej daty, oraz ustawienie na + 14 max
+
+  const dataInput = document.getElementById("data").value;
+  const finalData = document.querySelector(".finalData");
+
+  const data = new Date(dataInput);
+  const today = new Date();
+  const dataMax = new Date(today);
+  dataMax.setDate(dataMax.getDate() + 14);
+
+  // PODMIANA HTML W DIV o class our_machine_photo
+
+  machineImg.innerHTML = `<img src="${image}" class="img-fluid" alt="your new machine""/>`;
+
+  // Sprawdzanie poprawnosci wypelnienia formularza
+
+  if (
+    value.includes(" ") &&
+    regex.test(value) &&
+    data < dataMax &&
+    (radioButton1.checked || radioButton2.checked)
+  ) {
     errorName.style.display = "none";
     finalSection.classList.remove("hidden");
     finalSection.classList.add("visible");
     formSection.classList.remove("visible");
     formSection.classList.add("hidden");
-  } else {
+    finalData.textContent = data.toLocaleDateString();
+    localStorage.setItem("fullName", value);
+  } else if (!value.includes(" ") && !regex.test(value)) {
     errorName.style.display = "block";
-  }
+  } else if (data > dataMax) {
+    dataName.style.display = "block";
+  } else if (!radioButton1.checked || !radioButton2.checked) {
+    paymentError.style.display = "block";
+  } else paymentError.style.display = "none";
+
+  localStorage.setItem("fullName", name.value);
+  const nameFull = localStorage.getItem("fullName");
+  name.value = nameFull;
 });
 
-const backBtnFinal = document.querySelector(".backBtnFinal");
+// RESET PO ZAKUPIE
 
 backBtnFinal.addEventListener("click", () => {
   productSection.classList.add("visible");
@@ -129,4 +180,7 @@ backBtnFinal.addEventListener("click", () => {
   addGps.classList.remove("green");
   gpsIsAdded = true;
   cameraIsAdded = true;
+
+  // KASOWANIE LOCAL STORAGE
+  localStorage.removeItem("fullName");
 });
